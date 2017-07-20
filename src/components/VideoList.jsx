7d0d722fa397aed074ea4ec53482e006f01cffab
Hack Reactor Student <student@hackreactor.com>
2017-07-19 19:52:25 -0700

    Work on React sprint

--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,11 +1,18 @@
-var VideoList = () => (
+var VideoList = (props) => (
+  (props.videos.length === 0) ? <div>Loading Video List</div> : 
   <div className="video-list">
+    {/*<div><h5><em>videoListEntry</em> view goes here</h5></div>
     <div><h5><em>videoListEntry</em> view goes here</h5></div>
     <div><h5><em>videoListEntry</em> view goes here</h5></div>
     <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
+    <div><h5><em>videoListEntry</em> view goes here</h5></div>*/}
+    {console.log(props, 'videoList')}
+    {props.videos.map((item) => (<VideoListEntry clickHandeler = {props.clickHandeler} video = {item}/>) )}
+
   </div>
+
+
+
 );
 
 // PropTypes tell other developers what `props` a component expects
@@ -16,4 +23,8 @@ VideoList.propTypes = {
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope.
 // `var` declarations will only exist globally where explicitly defined.
-window.VideoList = VideoList;
+// window.VideoList = VideoList(videos = exampleVideoData);
+ReactDOM.render(
+  <VideoList videos= {window.exampleVideoData} />, 
+  document.getElementById('app')
+);
--- src/components/VideoPlayer.jsx
+++ src/components/VideoPlayer.jsx
@@ -1,13 +1,16 @@
-var VideoPlayer = () => (
-  <div className="video-player">
-    <div className="embed-responsive embed-responsive-16by9">
-      <iframe className="embed-responsive-item" src="https://www.youtube.com/embed/dQw4w9WgXcQ?autoplay=1" allowFullScreen></iframe>
+var VideoPlayer = (props) => (
+
+  (!props.video) ? <div>Loading Video Player</div> : (
+    <div className="video-player">
+      <div className="embed-responsive embed-responsive-16by9">
+        <iframe className="embed-responsive-item" src= {'https://www.youtube.com/embed/' + props.video.id.videoId} allowFullScreen></iframe>
+      </div>
+      <div className="video-player-details">
+        <h3>{props.video.snippet.title}</h3>
+        <div>{props.video.snippet.description}</div>
+      </div>
     </div>
-    <div className="video-player-details">
-      <h3>Video Title</h3>
-      <div>Video Description</div>
-    </div>
-  </div>
+  )
 );
 
 // PropTypes tell other developers what `props` a component expects
@@ -19,3 +22,5 @@ VideoPlayer.propTypes = {
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
 window.VideoPlayer = VideoPlayer;
+
+
